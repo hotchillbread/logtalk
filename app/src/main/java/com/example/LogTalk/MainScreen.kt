dc244b34988271bbafb63e15019d1b4aa60ee4d4
hotchillbread <kimbap0114@gmail.com>
2025-11-14 19:21:16 +0900

    home-16 과 병합

--- app/src/main/java/com/example/LogTalk/MainScreen.kt
+++ app/src/main/java/com/example/LogTalk/MainScreen.kt
@@ -1,31 +1,22 @@
-package com.example.logtalk.ui.navigation
+package com.example.logtalk
+
 
 import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Chat // 예시 아이콘
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings // 예시 아이콘
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,8 +29,6 @@ import com.example.logtalk.ui.chat.ChatScreen
 import com.example.logtalk.ui.chat.Message
 import com.example.logtalk.ui.theme.LoginColors
 import com.example.logtalk.ui.home.HomeScreen
-// 임시 화면
-@Composable fun HomeScreen() { Text("홈 화면") }
 
 @Composable fun SettingsScreen() { Text("설정 화면") }
 
