6b65c83916144d327c94dcf19d92c089de266ae5
hotchillbread <kimbap0114@gmail.com>
2025-11-25 18:42:56 +0900

    채팅 llm 로직 작성

--- app/src/main/java/com/example/logtalk/App.kt
+++ app/src/main/java/com/example/logtalk/App.kt
@@ -2,8 +2,6 @@ package com.example.logtalk
 
 import android.app.Application
 import com.example.logtalk.core.utils.Logger
-import com.example.logtalk.BuildConfig
-import com.google.android.gms.tasks.Tasks.await
 import dagger.hilt.android.HiltAndroidApp
 import com.example.logtalk.config.EnvManager
 import com.example.logtalk.core.utils.model.DependencyContainer
@@ -19,14 +17,15 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        //빌드 환경
         Logger.init(BuildConfig.DEBUG)
-
+        //env 주입(openai api key)
         EnvManager.initialize { isSuccessful -> 
             if (isSuccessful) {
                 container = DependencyContainer(EnvManager)
                 Logger.d("Dependency 초기화 완료")
             } else {
-                Logger.e("Remote config 로드 실패 DI 컨테이너 의존성 주입 안됨 ㅠ (다시 켜보든가)")
+                Logger.e("Remote config 로드 실패 DI 컨테이너 의존성 주입 안됨 (다시 켜보든가)")
             }
         }
     }
